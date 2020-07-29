26c50138328e0729f2b1076a48a3813b94ecbcff
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-07-29 15:03:29 -0300

    Listagem e adicionar novo item no carrinho

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -36,9 +36,22 @@ const CartProvider: React.FC = ({ children }) => {
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async product => {
+      const prod = products.find(p => p.id === product.id);
+
+      if (prod) {
+        setProducts(
+          products.map(p =>
+            p.id === product.id ? { ...product, quantity: p.quantity + 1 } : p,
+          ),
+        );
+      } else {
+        setProducts([...products, { ...product, quantity: 1 }]);
+      }
+    },
+    [products],
+  );
 
   const increment = useCallback(async id => {
     // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
